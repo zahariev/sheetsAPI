5a45b2d04c71d845ce3ae2cb06332c1e194063a8
b.zahariev <zaharievboyan@gmail.com>
2020-07-26 18:53:15 +0300

    fix: connection string from settings

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace sheetsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>();
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             //   x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             services.AddControllers();
@@ -48,7 +48,7 @@ namespace sheetsApi
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
