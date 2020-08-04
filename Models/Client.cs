cca79300e1be2a75c9e97640e5de0a1573ab0a77
b.zahariev <zaharievboyan@gmail.com>
2020-08-04 04:55:19 +0300

    fix: client model

--- Models/Client.cs
+++ Models/Client.cs
@@ -4,14 +4,8 @@ namespace sheetsApi.Models
 
     public class Client
     {
-        public Client()
-        {
-            Projects = new List<Project>();
-        }
-
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Project> Projects { get; set; }
     }
 }
